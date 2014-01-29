b989b69bc5d28944c52e0b8bec24ad8d9fa90ece
Oleg Temnov <otemnov@academixdirect.com>
2014-01-30 03:39:35 +0400

    Configuration for ignore excessively long/wide images ration

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     url = request.args.get('url', '')
-    a = Article(url)
+    a = Article(url, image_dimension_ration = 3)
     a.download()
     a.parse()
     a.nlp()
